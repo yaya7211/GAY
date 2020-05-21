1a44f2d851aa4e737118d7f38715af88c08a244c
yaya7211 <43450496+yaya7211@users.noreply.github.com>
2020-05-21 13:30:54 +0000

    Update GAY.py

--- GAY.py
+++ GAY.py
@@ -1,179 +1,75 @@
 import collections
 
-class UnfoundElementError(Exception):
+class inValidKeyError(Exception):
 	pass
 
 class GAY:
-	def __init__(self):
-		self.key = []
+	def __init__(self, key):
+		self.key = key
 
 	def cipher(self, word):
-		"""Cyphering method, use like GAY.cypher(\"wordToCypher\"), after setting the key in GAY().key"""
 		try:
-			alphabet = self.key[0]
-			msg = word
-			lst = []
-			a = 0
-			mot = 1
-			while a != len(msg) :
-			    mot = mot * alphabet[msg[a]]
-			    a += 1
-			######From Kwak######
+			final = 1
+			for x in range(len(message)):
+				final *= key[word[x]]
 			traduction_char = collections.defaultdict(list)
 			swap = lambda tup: (tup[1], tup[0])
 			char_index = dict(map(swap, enumerate(order)))
-			for i, ch in enumerate(sorted(msg, key=lambda ch: char_index[ch])):
+			for i, ch in enumerate(sorted(word, key=lambda ch: char_index[ch])):
 				traduction_char[ch].append(i)
-			######From Kwak######
-			ordre = list(map(lambda ch: traduction_char[ch].pop(), msg))
-			for x in range(0, len(ordre)):
-				ordre[x] = str(ordre[x])
-			return str(mot)+"/"+".".join(ordre)
+			ordre = list(map(lambda ch: traduction_char[ch].pop(), word))
+			return final + "/" + "."join([str(x) for x in ordre])
 		except KeyError:
-			raise UnfoundElementError("A caracter is missing in key.")
+			for x in word:
+				if !(x in self.key):
+					raise inValidKeyError("A caracter \""+ x +"\" is missing in the key used to cipher")
 
 	def decipher(self, word):
-		"""Decyphering method, use like GAY().decypher(\"wordtodecypher\"), after setting the key in GAY().key"""
 		try:
-			alphabet = self.key[1]
-			msg = int(word.split("/")[0])
-			ordre = word.split("/")[1].split(".")
-			for x in range(0,len(ordre)):
-				ordre[x] = int(ordre[x])
-			######From stackoverflow######
-			msgF =[]
-			d = 2
-			while msg > 1 :
-			    while msg % d==0 :
-			        msg = msg // d
-			        msgF.append(d)
-			    d += 1
-			######From stackoverflow######
-			d = 0
-			while d != len(msgF):
-			    msgF[d] = alphabet[msgF[d]]
-			    d += 1
+			ordre = [int(x) for x in word.split("/")[1]]
+			word = int(word.split("/")[0])
+			word = decomp(word)
 			final = ""
-			d = 0
-			while d != len(msgF):
-			    final += msgF[int(ordre[d])]
-			    d += 1
+			for x in word:
+				word = self.key.index(x)
+			word, final = final, ""
+			for x in len(final):
+				final += word[ordre[x]]
 			return final
 		except KeyError:
-			raise UnfoundElementError("A primary number is missing in key.")
+			raise inValidKeyError("A primary number is missing in the key used to decipher")
 
-	def hash(self, word):
-		try:
-			alphabet = self.key[0]
-			msg = word
-			lst = []
-			a = 0
-			mot = 1
-			while a != len(msg) :
-			    mot = mot * alphabet[msg[a]]
-			    a += 1
-			######From Kwak######
-			traduction_char = collections.defaultdict(list)
-			swap = lambda tup: (tup[1], tup[0])
-			char_index = dict(map(swap, enumerate(order)))
-			for i, ch in enumerate(sorted(msg, key=lambda ch: char_index[ch])):
-				traduction_char[ch].append(i)
-			######From Kwak######
-			ordre = list(map(lambda ch: traduction_char[ch].pop(), msg))
-			for x in range(0, len(ordre)):
-				ordre[x] = str(ordre[x])
-			ordre = "".join(ordre)
-			mot = str(mot)
-			x = len(mot)
-			y = len(ordre)
-			final = ""
-			a = pgcd(x,y)
-			if a != 1:
-				x = x // a
-				y = y // a
-				for e in range(a):
-					final += mot[:x-1]
-					mot = mot[x-1:]
-					final += ordre[:y-1]
-					ordre = ordre[y-1:]
-			else:
-				mot = mot[:len(mot)//2]
-				f = list(zip(mot, ordre))
-				for x in f:
-					for y in x:
-						final += y
-			return final
-		except KeyError:
-			raise UnfoundElementError("A primary number is missing in key.")
+def decomp(word):
+	x = list(filter((lambda y: not(word % y)), range(2, word+1)))
+	return [x[0]]+decomp(word//x[0]) if x else []
 
+def isPrime(num):
+	return True if not list(filter((lambda y: not(word % y)), range(2, round(word**0.5)))) else False
 
-
-def pgcd(a,b) :  
-	while a%b != 0 : 
-		a, b = b, a%b 
-	return b
-
-def isPrime(prime):
-	"""Plz don't use this func, it's for the dev"""
-	lst = []
-	d = 2
-	while d <= prime ** 0.5 :
-		if prime % d != 0:
-			pass
-		elif prime % d == 0:
-			return False
-		d += 1
-	return True
-
-def mDic(primes, charset):
-	"""Plz don't use this func, it's for the dev"""
-	dic = {}
-	a = 0
-	while a != len(charset):
-		dic.update({charset[a]:int(primes[a])})
-		a += 1
-	final = []
-	final.append(dic)
-	dic = {}
-	a = 0
-	while a != len(charset):
-		dic.update({int(primes[a]):charset[a]})
-		a += 1
-	final.append(dic)
-	global order 
-	order = charset
-	return final
-
-def key_gen(charset, prim):
-	"""Key generator, use like KeyGen([yourCharset], [anInteger])"""
+def key_gen(order, passwd):
 	if len(list(charset)) != len(set(list(charset))):
-		raise InvalidKeyFormatError("Caracter should not have more than one occurrence.")
-	global order #cipher
-	global char
-	char = charset
-	order = list(charset)
+		raise inValidKeyError("Caracter should not have more than one occurrence.")
+	global order
 	global primes
 	primes = []
-	while len(primes) != len(charset):
+	while len(primes) != len(order):
 		if isPrime(prim):
 			primes.append(prim)
 		prim += 1
-	return mDic(primes, charset)
+	return mDic(primes, order)
+
+def mDic(primes, charset):
+	final = {}
+	for x in range(len(charset)):
+		final.update({charset[a]:int(primes[a])})
+	return final
 
 def save_key():
-	f = open("key.key", "w")
-	f.write(char+"\n")
-	p = ""
-	for x in range(len(primes)):
-		p += str(primes[x])+" "
-	f.write(p)
-	f.close()
+	f = open('key.key', 'w')
+	f.write(order+"\n")
+	for x in primes:
+		f.write(x + " ")
 
 def use_key(path):
-	f = open(path, "r")
-	a = f.read()
-	charset = a.split("\n")[0]
-	primes = a.split("\n")[1]
-	primes = primes.split(" ")
-	primes.pop(len(primes) - 1)
-	return mDic(primes, charset)
+	f = open(path, 'r').readlines()
+	return mDic(f[1].split(" "), f[0])
